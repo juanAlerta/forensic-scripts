19ea1ed0c49ad8da9476a99558c29970dcf70339
Juan Blanco Martín <39630992+juanAlerta@users.noreply.github.com>
2024-04-19 18:54:32 +0200

    Add files via upload

---
+++ hash_dir.py
@@ -0,0 +1,27 @@
+import hashlib
+import os
+
+def calculate_file_hashes(directory):
+	hashes = {}
+
+	for file in os.listdir(current_dir):
+		# filepath = os.path.join(directory, filename)
+		if os.path.isfile(file):
+			with open(file, 'rb') as f:
+				sha1 = hashlib.sha1()
+				sha1.update(f.read())
+				hashes[f] = sha1.hexdigest()
+
+	return hashes
+
+current_dir = os.getcwd()
+file_hashes = calculate_file_hashes(current_dir)
+print(file_hashes)
+
+# TO-DO 
+# -----------------------------------------------------------
+# Solicitar ruta cuando se ejecuta el script
+# Incorporar API VT
+# Automatizar búsqueda en VT
+# -----------------------------------------------------------
+
---
+++ hash_dir.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -a hashes
+
+for file in * 
+do 
+	if [ -f "$file" ]; then
+		hash=$(sha1sum "$file")
+		hashes+=("$hash")
+	fi
+done
+
+for i in "${hashes[@]}"; do
+	echo "$i"
+done
+
+# TO-DO 
+# -----------------------------------------------------------
+# Solicitar ruta cuando se ejecuta el script
+# Replicar script en Python ✅
+# -----------------------------------------------------------
