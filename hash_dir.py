ef48f459666c42456fce7e6dfe70915e0ea2f100
juan <metridxblack@gmail.com>
2024-04-25 00:51:33 +0200

    Colección diccionario y .name en key

--- hash_dir.py
+++ hash_dir.py
@@ -1,30 +1,36 @@
 import hashlib
 import os
 import requests
+import sys
 
 def calculate_file_hashes(directory):
 	hashes = {}
 
 	for file in os.listdir(current_dir):
-		# filepath = os.path.join(directory, filename)
 		if os.path.isfile(file):
 			with open(file, 'rb') as f:
 				sha1 = hashlib.sha1()
 				sha1.update(f.read())
-				hashes[f] = sha1.hexdigest()
+				hashes[f.name] = sha1.hexdigest()
 
 	return hashes
 
-current_dir = os.getcwd()
+if len(sys.argv) > 1:
+	current_dir = sys.argv[1]
+else:
+	current_dir = os.getcwd()
+
 file_hashes = calculate_file_hashes(current_dir)
-print(file_hashes)
+for key in file_hashes.keys():
+    print(key, ":", file_hashes[key])
+
 
 
 # 🌐VirusTotal diagnosis
-api_key = 63df35d4d960b46bfccc0a9ff630bde361edf5a5075ee46474e7bee603077cf6
+api_key = "63df35d4d960b46bfccc0a9ff630bde361edf5a5075ee46474e7bee603077cf6"
 
 # definir hash_id
-url = "https://www.virustotal.com/api/v3/files/id" + hash_id
+url = "https://www.virustotal.com/api/v3/files/id" #+ hash_id
 
 
 
