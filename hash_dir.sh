27cf5ebe000b8222565c5d0e4b68b63ff85f85ca
juan <metridxblack@gmail.com>
2025-04-03 16:04:32 +0200

    fix merge

--- hash_dir.sh
+++ hash_dir.sh
@@ -1,38 +1,10 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-declare -a hashes
-
-for file in * 
-do 
-	if [ -f "$file" ]; then
-		hash=$(sha1sum "$file")
-		hashes+=("$hash")
-	fi
-done
-
-for i in "${hashes[@]}"; do
-	echo "$i"
-done
-
-# TO-DO 
-# -----------------------------------------------------------
-# Solicitar ruta cuando se ejecuta el script
-# Replicar script en Python ✅
-# -----------------------------------------------------------
-=======
 #!/bin/bash
 declare -a hashes
 
 for file in * 
 do 
 	if [ -f "$file" ]; then
-		hash=$(sha1sum "$file")
+		hash=$(sha256sum "$file")
 		hashes+=("$hash")
 	fi
 done
-
-for i in "${hashes[@]}"; do
-	echo "$i"
-done
->>>>>>> d21020ed9209d0e1f3230023431d6974b75cd7d5
